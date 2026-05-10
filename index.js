aba009081f27281c01a95554f0f50dc951cb97a0
VusanDebnath <sm.vusandebnath@gmail.com>
2026-05-10 19:29:24 +0600

    fix: CORS updated for production

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://home-service-management-server.onrender.com",
     credentials: true,
   }),
 );
